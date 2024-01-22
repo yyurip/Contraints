5bb1bf263a860e0b926eabe9f5db9b6264731864
Ygor Pessoa <ygoryuri@deliveryhero.com>
2024-01-22 10:51:47 +0100

    Updated files

---
+++ Constraints/Constraints.h
@@ -0,0 +1,18 @@
+//
+//  Constraints.h
+//  Constraints
+//
+//  Created by Ygor Yuri De Pinho Pessoa on 22.01.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Constraints.
+FOUNDATION_EXPORT double ConstraintsVersionNumber;
+
+//! Project version string for Constraints.
+FOUNDATION_EXPORT const unsigned char ConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Constraints/PublicHeader.h>
+
+
